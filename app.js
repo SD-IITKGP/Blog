1b47df7c5a8f00568884e537078faeac75d16396
codercool <jaihindsourabh@kgpian.iitkgp.ac.in>
2022-12-09 08:44:34 +0000

    Blog version 2.0.0

--- app.js
+++ app.js
@@ -2,51 +2,83 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const lodash = require("lodash");
 const ejs = require("ejs");
 const app = express();
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose.set("strictQuery", true);
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-const posts=[];
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-app.get("/",function(req,res){
-  res.render("home",{homeStartingContent:homeStartingContent,posts:posts,});
+mongoose.connect(
+  "mongodb+srv://sourabhchoudhary:Sourabh1503.@cluster0.hch1sgl.mongodb.net/postDB",
+  { useNewUrlParser: true }
+);
+const homeStartingContent =
+  "This is my personal Blog. I will be posting my thoughts regularly using this page. Hope u like it!! I have tried to keep this site as simple and sleek as possible as I wanted to keep it exclusively for sharing my thoughts without publishing any unwanted things:).";
+const aboutContent =
+  "My name is Sourabh Choudhary. I am a third year undergraduate student at IIT Kharagpur. To know more about please have a look at my website sourabhchoudhary.live by clicking on the link at the fotter of this website. Also any of the posts on this websites are just my point of views, it does not targets or points to any individual or organisation.";
+const contactContent =
+  "For contacting me you can reach me out at my email - contact@sourabhchoudhary.live. Also if you have any suggestion, feel free to reach out to me at this email, I will try to reach out to u as soon as possible. :)";
+const postSchema = mongoose.Schema({
+  title: String,
+  body: String,
 });
-app.get("/about",function(req,res){
-  res.render("about",{aboutContent:aboutContent});
+const Post = mongoose.model("Post", postSchema);
+app.get("/", function (req, res) {
+  Post.find({}, function (err, posts) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {
+        homeStartingContent: homeStartingContent,
+        posts: posts,
+      });
+    }
+  });
 });
-app.get("/contact",function(req,res){
-  res.render("contact",{contactContent:contactContent});
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
 });
-app.get("/compose",function(req,res){
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
-app.get("/posts/:type",function(req,res){
-  if(posts.length>0){
-    posts.forEach(function(post){
-      if(lodash.lowerCase(post.title)===lodash.lowerCase(req.params.type)){
-        res.render("post",{post:post});
+app.get("/posts/:type", function (req, res) {
+  Post.find({}, function (err, posts) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (posts.length > 0) {
+        posts.forEach(function (post) {
+          if (
+            lodash.lowerCase(post.title) === lodash.lowerCase(req.params.type)
+          ) {
+            res.render("post", { post: post });
+          } else {
+            res.render("post", {
+              post: { title: "Error: 404", body: "Not Found :(" },
+            });
+          }
+        });
+      } else {
+        res.render("post", {
+          post: { title: "Error: 404", body: "Not Found :(" },
+        });
       }
-      else{
-        res.render("post",{post:{title:"Error: 404", body:"Not Found :("},});
-      }
-    });
-  }
-  else{
-    res.render("post",{post:{title:"Error: 404", body:"Not Found :("},});
-  }
+    }
+  });
 });
-app.post("/compose",function(req,res){
-  const post={
-    title:req.body.composeTitle,
-    body:req.body.composeText,
-  };
-  posts.push(post);
+app.post("/compose", function (req, res) {
+  const post = new Post({
+    title: req.body.composeTitle,
+    body: req.body.composeText,
+  });
+  post.save();
   res.redirect("/");
 });
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
